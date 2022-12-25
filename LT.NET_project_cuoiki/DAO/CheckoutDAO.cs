d3632dd28ad4ce1b134f0c068750b121506466f9
Nguyễn Phi Khanh <20130288@st.hcmuaf.edu.vn>
2022-12-25 14:20:00 +0700

    cm

---
+++ LT.NET_project_cuoiki/DAO/CheckoutDAO.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace LT.NET_project_cuoiki.DAO
+{
+    public class CheckoutDAO
+    {
+        public void addCheckout(string fullname, string hnum, string ward, string county, string province,string mail,string phone, string note)
+        {
+            string query_order = "";
+            string query_order_detail = "";
+            string query_address = "INSERT INTO address(hnum_sname,ward_commune,county_district,province_city) VALUES (@hnum,@ward,@county,@province)";
+
+            MySqlConnection mySqlConnection = new ConnectionMysql().OpenConnection();            
+
+            MySqlCommand commandAddress= new MySqlCommand();
+            commandAddress.Connection = mySqlConnection;
+            commandAddress.CommandText = query_address;
+
+            commandAddress.Parameters.AddWithValue("@hnum", hnum);
+            commandAddress.Parameters.AddWithValue("@ward", ward);
+            commandAddress.Parameters.AddWithValue("@county", county);
+            commandAddress.Parameters.AddWithValue("@province", province);
+
+            commandAddress.ExecuteNonQuery();
+
+
+        }
+    }
+}
---
+++ LT.NET_project_cuoiki/Models/CheckoutModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LT.NET_project_cuoiki.Models
+{
+    public class CheckoutModel
+    {
+        private string name,hnum,ward,county,province,mail,phone,note;
+
+        public CheckoutModel()
+        {
+        }
+
+        public CheckoutModel(string name, string hnum, string ward, string county, string province, string mail, string phone, string note)
+        {
+            this.Name = name;
+            this.Hnum = hnum;
+            this.Ward = ward;
+            this.County = county;
+            this.Province = province;
+            this.Mail = mail;
+            this.Phone = phone;
+            this.Note = note;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Hnum { get => hnum; set => hnum = value; }
+        public string Ward { get => ward; set => ward = value; }
+        public string County { get => county; set => county = value; }
+        public string Province { get => province; set => province = value; }
+        public string Mail { get => mail; set => mail = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public string Note { get => note; set => note = value; }
+    }
+}
